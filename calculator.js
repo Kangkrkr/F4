83bfc53444f9a571e0588b5f1a669b9eb27362c4
kangkrkr <kangkrkr@naver.com>
2016-12-09 15:43:10 +0900

    modify all function

--- calculator.js
+++ calculator.js
@@ -1,18 +1,22 @@
 function calculator(operator, left, right){
 	
+	var intLeft = parseInt(left);
+	var intRight = parseInt(right);
+
 	// 더하기 
-	if(operator === '+')
-		return left + right;
+	if(operator === '+'){
+		return intLeft + intRight;
+	}
 
 	// 빼기
 	if(operator === '-')
-		return left - right;
+		return intLeft - intRight;
 
 	// 나눗셈
 	if(operator === '/')
-		return left/right;
+		return intLeft/intRight;
 
 	// 곱하기
 	if(operator === '*')
-		return left*right;
+		return intLeft*intRight;
 }
