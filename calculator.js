15f7751609b70954af7c8d2885d6674ff9c46345
kangkrkr <kangkrkr@naver.com>
2016-12-09 15:05:31 +0900

    add calculator.js

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function calculator(oprator, left, right){
+	
+	// 더하기 
+	if(oprator === '+')
+		return left + right;
+
+	// 빼기
+	if(oprator === '-')
+		return left - right
+
+	// 나눗셈
+
+	// 곱하기
+
+}
